8be380144d99fb94c22c15a59e5791043a8df054
Alejo <AlejoUtnMartina@gmail.com>
2023-07-12 10:25:49 -0300

    Signed-off-by: Alejo <AlejoUtnMartina@gmail.com>
    added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //my firs change
+        //my second change
         [Key]
         public int CartItemId { get; set; }
 
